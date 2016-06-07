209335d026bc9f89bc6e8e98147584a951fd5ca3
qinjiangwei <qinjiangwei@uascent.com>
2016-06-07 12:19:42 +0800

    mouse read

--- app/src/main/java/com/uascent/android/pethunting/devices/BluetoothAntiLostDevice.java
+++ app/src/main/java/com/uascent/android/pethunting/devices/BluetoothAntiLostDevice.java
@@ -20,20 +20,26 @@ public class BluetoothAntiLostDevice extends BluetoothLeClass {
     public static final UUID BATTERY_SERVICE_UUID = UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb");
     public static final UUID BATTERY_FUNC_UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb");
 
-    public static final UUID KEY_FUNC1_UUID = UUID.fromString("0000fff1-0000-1000-8000-00805f9b34fb");
-    public static final UUID KEY_FUNC2_UUID = UUID.fromString("0000fff2-0000-1000-8000-00805f9b34fb");
-    public static final UUID KEY_SERVICE1_UUID = UUID.fromString("0000fff0-0000-1000-8000-00805f9b34fb");
-
-    //自定义服务命令通道
-
-//    public static final UUID KEY_FUNC_UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
-//    public static final UUID KEY_SERVICE_UUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
+    public static final UUID KEY_FUNC_UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
+    public static final UUID KEY_SERVICE_UUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
 
     public static final UUID ALERT_SERVICE_UUID = UUID.fromString("00001802-0000-1000-8000-00805f9b34fb");
     public static final UUID ALERT_FUNC_UUID = UUID.fromString("00002a06-0000-1000-8000-00805f9b34fb");
 
     public static final UUID LOSS_SERVICE_UUID = UUID.fromString("00001803-0000-1000-8000-00805f9b34fb");
     public static final UUID LOSS_FUNC_UUID = UUID.fromString("00002a06-0000-1000-8000-00805f9b34fb");
+
+    public static final UUID MOUSE_WRITE_FUNC_UUID = UUID.fromString("0000ffe2-0000-1000-8000-00805f9b34fb");
+    public static final UUID MOUSE_READ_FUNC_UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
+    public static final UUID MOUSE_SERVICE_UUID = UUID.fromString("0000fff0-0000-1000-8000-00805f9b34fb");
+
+
+
+    public static final int MOUSE_UP = 1;
+    public static final int MOUSE_DOWN = 2;
+    public static final int MOUSE_LEFT = 3;
+    public static final int MOUSE_RIGHT = 4;
+
 
     //    public static final int ALERT_ON = 2;
 //    public static final int ALERT_OFF = 0;
@@ -127,6 +133,55 @@ public class BluetoothAntiLostDevice extends BluetoothLeClass {
 //
 //        readCharacteristic(characteristic);
 //    }
+
+    public void enableKeyReport(boolean on) {
+        Log.e(TAG, "enable notification");
+
+        if (!checkBleStatus()) {
+            return;
+        }
+
+        //接受Characteristic被写的通知,收到蓝牙模块的数据后会触发mOnDataAvailable.onCharacteristicWrite()
+        setCharacteristicNotification(KEY_SERVICE_UUID, KEY_FUNC_UUID, on);
+
+        BluetoothGattService service = mBluetoothGatt.getService(KEY_SERVICE_UUID);
+        if (service == null) {
+            return;
+        }
+
+        BluetoothGattCharacteristic characteristic = mBluetoothGatt.getService(KEY_SERVICE_UUID).getCharacteristic(KEY_FUNC_UUID);
+
+        if (characteristic != null) {
+            characteristic.setValue("send data->");
+            //往蓝牙模块写入数据
+            writeCharacteristic(characteristic);
+        }
+    }
+
+    public void mouseControl(int direction) {
+        Log.e(TAG, "mouseControl");
+
+        if (!checkBleStatus()) {
+            return;
+        }
+
+        //接受Characteristic被写的通知,收到蓝牙模块的数据后会触发mOnDataAvailable.onCharacteristicWrite()
+        setCharacteristicNotification(KEY_SERVICE_UUID, KEY_FUNC_UUID, true);
+
+        BluetoothGattService service = mBluetoothGatt.getService(MOUSE_SERVICE_UUID);
+        if (service == null) {
+            return;
+        }
+
+        BluetoothGattCharacteristic characteristic = mBluetoothGatt.getService(MOUSE_SERVICE_UUID).getCharacteristic(MOUSE_WRITE_FUNC_UUID);
+
+        if (characteristic != null) {
+            characteristic.setValue("send data->");
+            //往蓝牙模块写入数据
+            writeCharacteristic(characteristic);
+        }
+    }
+
 
     public void setImmediateAlert(int val) {
         Lg.i(TAG, "setImmediateAlert() value = " + val);
