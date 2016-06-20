27f55f5d13301f1d2fe41399bd8a5243aa2f7b35
lizhigang <320372414@qq.com>
2016-06-20 19:37:15 +0800

    设置全局字体样式

--- app/src/main/java/com/uascent/android/pethunting/MyApplication.java
+++ app/src/main/java/com/uascent/android/pethunting/MyApplication.java
@@ -2,7 +2,9 @@ package com.uascent.android.pethunting;
 
 import android.app.Activity;
 import android.app.Application;
+import android.graphics.Typeface;
 
+import java.lang.reflect.Field;
 import java.util.LinkedList;
 
 /**
@@ -12,10 +14,12 @@ public class MyApplication extends Application {
     public LinkedList<Activity> activityList = new LinkedList<Activity>();
     private static MyApplication instance;
     public boolean isAutoBreak = false;
+    public static Typeface typeFace;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        setTypeface();
         //异常重启
 //        AppCrashHandler ch = AppCrashHandler.getInstance();
 //        ch.init(getApplicationContext());
@@ -34,6 +38,21 @@ public class MyApplication extends Application {
 
     public void removeActivity(Activity activity) {
         activityList.remove(activity);
+    }
+
+    public void setTypeface() {
+//        typeFace = Typeface.createFromAsset(getAssets(), "fonts/aaa.ttf");
+        typeFace = Typeface.createFromAsset(getAssets(), "fonts/test.ttf");
+
+        try {
+            Field field = Typeface.class.getDeclaredField("SERIF");
+            field.setAccessible(true);
+            field.set(null, typeFace);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 
