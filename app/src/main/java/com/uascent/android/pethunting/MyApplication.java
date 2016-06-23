d4d6098a1582b88bb174e0a87040f450e0a35947
lizhigang <320372414@qq.com>
2016-06-23 13:37:21 +0800

    修改字体样式 稳定版

--- app/src/main/java/com/uascent/android/pethunting/MyApplication.java
+++ app/src/main/java/com/uascent/android/pethunting/MyApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.graphics.Typeface;
 
 import com.uascent.android.pethunting.tools.AppCrashHandler;
+import com.uascent.android.pethunting.tools.Lg;
 
 import java.lang.reflect.Field;
 import java.util.LinkedList;
@@ -13,6 +14,7 @@ import java.util.LinkedList;
  * Created by lenovo001 on 2016/5/24.
  */
 public class MyApplication extends Application {
+    private static final String TAG = "MyApplication";
     public LinkedList<Activity> activityList = new LinkedList<Activity>();
     private static MyApplication instance;
     public boolean isAutoBreak = false;
@@ -45,16 +47,20 @@ public class MyApplication extends Application {
 
     public void setTypeface() {
 //        typeFace = Typeface.createFromAsset(getAssets(), "fonts/aaa.ttf");
-        typeFace = Typeface.createFromAsset(getAssets(), "fonts/test.ttf");
-
+//        typeFace = Typeface.createFromAsset(getAssets(), "fonts/test.ttf");
+        typeFace = Typeface.createFromAsset(getAssets(), "fonts/arial.ttf");
+        Lg.i(TAG, "setTypeface()");
         try {
             Field field = Typeface.class.getDeclaredField("SERIF");
             field.setAccessible(true);
             field.set(null, typeFace);
+            Lg.i(TAG, "setTypeface()_suceess");
         } catch (NoSuchFieldException e) {
+            Lg.i(TAG, "setTypeface()_suceess_fail1");
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
+            Lg.i(TAG, "setTypeface()_suceess_fail2");
         }
     }
 
